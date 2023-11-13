b3622612c93a6e216737d0fb8c91acf0f116c39b
Tejas Nampalli <TSNampalli@gmail.com>
2023-11-12 21:59:01 -0500

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+function findMatching(drivers, string) {
+    return drivers.filter(function (driver) {
+        return driver.toLowerCase() === string.toLowerCase()
+    })
+}
+
+function fuzzyMatch(drivers, name) {
+    return drivers.filter (function(driver) {
+     return driver.slice(0,2) === name
+    })
+}
+
+
+function matchName(drivers, string) {
+    return drivers.filter(function(driver){
+        return driver.name === string
+
+    })
+
+}
